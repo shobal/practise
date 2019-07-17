8f1d7fb7d11c3677e8145e062bdacfb13598d0ff
guoxiaobao <guoxiaobao@yy.com>
2019-07-17 11:48:24 +0800

    compile kotlin

--- app/src/main/java/com/shobal/gradlepractise/MyContants.java
+++ app/src/main/java/com/shobal/gradlepractise/MyContants.java
@@ -4,10 +4,13 @@ import com.shobal.applib.OkCt;
 
 /**
  * Created by shobal on 18/4/18.
+ * @link{}
+ * @see MyContants
  */
 public class MyContants {
     public static String toastStr = "ty6yyu";
     String aa = OkCt.aacc;
     KtTestt bb;
     MyFirst myFirst;
+    int cck = R.id.my_toast;
 }
